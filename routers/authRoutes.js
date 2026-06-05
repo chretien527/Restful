fb5bc98d88e8ebe72a0a359c82ef8b2ef636ae29
NAMBAJIMANA SANO Chretien <chretiensano@gmail.com>
2026-06-05 08:20:20 +0200

    modifications

--- routers/authRoutes.js
+++ routers/authRoutes.js
@@ -1,15 +1,14 @@
 import express from "express";
 import User from "../models/User.js";
-import bcrypt from "bcrypt.js";
-import User from "../models/User.js";
+import bcrypt from "bcryptjs";
 
 const router = express.Router();
 
 router.post("/signup", async(req,res) => {
     try{
-        const { name, email, password} = req.body;
-
-        const existingUser = await User.findOne({email});
+        const { name, email, password } = req.body;
+        
+        const existingUser = await user.findOne({email});
 
         if(existingUser){
             return res.status(400).json({
@@ -17,20 +16,20 @@ router.post("/signup", async(req,res) => {
             });
         }
 
-        const hashedPassword = await bcrypt.hash(password, 20);
+        const hashedPassword = await bcrypt.hash(password,20);
 
-        const user = await User.create({
+        const user = await User.create([
             name,
             email,
-            password:hashedPassword,
-        });
+            password= hashedPassword,
+        ]);
 
-        return res.status(201).json({
+        res.status(500).json({
             message:"User created successfully",
-        });
+        })
     }catch(error){
         res.status(500).json({
-            message:"Inter server error"
+            message:'Internal server error',
         });
     }
 });
