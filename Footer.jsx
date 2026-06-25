aedea1d6c1a6fa82d810e9e6e35f32dcd5a9625b
NAMBAJIMANA SANO Chretien <chretiensano@gmail.com>
2026-06-25 20:10:11 +0200

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,16 @@
+import './Footer.css';
+
+function Footer(){
+    return(
+        <div>
+            <div>
+            <img src="../../public/footer1.png" alt="image" />
+            <h1>50+</h1>
+            <p>SIGNATURE ITEMS</p>
+            </div>
+            <div>
+            <img src="../../public/footer2.png" alt="image" />
+            </div>
+        </div>
+    )
+}
